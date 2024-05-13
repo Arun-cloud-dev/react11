d9ea802916d2d441ac27a4f771863503d6ca721c
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-05-13 21:54:45 +0530

    Initial commit

---
+++ 3/tic-tac-toe/src/index.js
@@ -0,0 +1,12 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Game';
+import './styles.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ README.md
@@ -0,0 +1,2 @@
+# react11
+ react
